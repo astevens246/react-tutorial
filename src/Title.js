e00d76fd4935b1f0711e5efe30e5ce8683ed6cea
Allen Stevens <allen.stevens@students.dominican.edu>
2024-02-08 11:07:26 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+// src/Title.js
+import './Title.css';
+import React from 'react';
+
+function Title() {
+    return (
+      <div className= "Title">
+        <h1>SFPOPOS</h1>
+        <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+
+      </div>
+    )
+  }
+  
+  export default Title
